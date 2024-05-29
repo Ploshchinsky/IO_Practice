fb246041e16ea1c0bb7fe820dc91764c00ca7488
Anton P. <made1of1fire@gmail.com>
2024-05-29 21:23:38 +0500

    fix(main-structure): updated main structure -> added resources and edited test naming

---
+++ src/test/java/org/textFile/TestTextFile.java
@@ -0,0 +1,13 @@
+package org.textFile;
+
+import org.junit.jupiter.api.Test;
+
+
+public class TestTextFile {
+    @Test
+    public void test1() {
+        TextFile file1 = new TextFile()
+    }
+}
+
+
